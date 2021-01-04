0f72fb09cbd07474f4b6ed27b795196bb61cd51d
Theeradon B <71056713+gufilmsamuiza@users.noreply.github.com>
2021-01-04 15:34:11 +0700

    Create Web.php

---
+++ Web.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  <style>
+    .navbar {
+      background-color: #4d94ff;
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }a{
+      color: white;
+    }
+    .img{
+      
+      background-color: white;
+      text-align: center;
+    }h2{
+      font-family: "Prompt",sans-serif !important;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"></button>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand " href="#" style="color: white;">ITD WU</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav" >
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <div class="container">
+    <div class="img"><img src="https://informatics.wu.ac.th/wp-content/uploads/69830446_2942107012472469_4166277130780409856_n-2.jpg" width="100%">
+  </div>
+  <h2>ITD เทคโนโลยีสารสนเทศและนวัตกรรมดิจิทัล</h2>
+  <br>
+  <p>หลักสูตรเทคโนโลยีสารสนเทศและนวัตกรรมดิจิทัลหรือเรียกสั้น ๆ ว่า เด็ก ITD สังกัดสาขาวิชาเทคโนโลยีสารสนเทศ สำนักวิชาสารสนเทศาสตร์ ม.วลัยลักษณ์ เป็นหลักสูตรปรับปรุงใหม่ โดยหลักสูตรของเราสอนให้นักศึกษาเป็นนักแก้ไขปัญหา (Problem Solver) เรียนรู้อย่างเข้มข้น  รู้ปัญหา เก็บข้อมูล วิเคราะห์และออกแบบระบบ สร้างสิ่งใหม่ ทำการทดสอบ และใช้งานจริง พวกเราคือนักพัฒนาตัวจริงแบบครบวงจร</p>
+</div>
+  
+
+
+
+
+</body>
+</html>
